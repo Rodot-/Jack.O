cdd0860f411f077dec13cee613ac590e4117a649
Jack O'Brien <jto33@drexel.edu>
2014-12-11 16:46:49 -0500

    Create WeaponBalanceTest.py
    Built framework for weapon testing and balancing.

---
+++ Balancing_Code/WeaponBalanceTest.py
@@ -0,0 +1,9 @@
+#Program Name: WeaponBalanceTest.py
+#Purpose: Platform for building weapon classes and testing results near infinity (or very large values)
+#Author: Jack O'Brien
+#Date of creation: 12/11/2014
+#Date of last edit: 12/11/2014
+
+from random import randint
+import matplotlib.pyplot as plt
+import numpy as np
