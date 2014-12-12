891995e0808e209a70dbc54edd2d34fb4ac9f9b0
Jack O'Brien <jto33@drexel.edu>
2014-12-11 19:33:07 -0500

    Updating Weapon Balancing Code.  Created Two Parsing Functions.

--- Balancing_Code/WeaponBalanceTest.py
+++ Balancing_Code/WeaponBalanceTest.py
@@ -7,3 +7,66 @@
 from random import randint
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+
+class Gun():
+
+	def __init__(Damage = '1d6+0', Category = 'M', Range = 40, Critical = '19-20x2'):
+
+		self.Damage = parseDamage(Damage)
+
+		self.Critical = parseCritical(Critical)
+
+		self.Category = 'M'
+
+		self.Range = Range
+
+
+def parseDamage(Damage, pos = 0): #Parses the Damage parameter for a weapon
+
+	n = int(Damage[:Damage.find('d')]) #Number of dice rolled
+
+	d = int(Damage[Damage.find('d')+1:Damage.find('+')]) #Number of sides on dice rolled
+
+	m = int(Damage[Damage.find('+')+1:]) #Modifier of the dice roll
+
+	def Output(): #Function used for determining result of weapon use
+
+		result = 0
+
+		for i in range(n):
+
+			result += randint(1,d)
+
+		result += m
+
+		return result
+	
+	return Output
+
+
+
+def parseCritical(Critical): #Parses the Critical parameter for a weapon
+
+	if Critical.find('-') != -1:
+
+		threatRange = int(Critical[:Critical.find('-')]) #Sets the threat range for a critical hit
+
+	else:
+
+		threatRange = 20
+	
+	n = int(Critical[Critical.find('x'):]) #Critical Multiplier
+
+	def detectCritical(RawRoll): #Function for determining critical hit result
+
+		if RawRoll >= threatRange:
+
+			return n
+
+		else:
+
+			return 0
+
+	return detectCritical
